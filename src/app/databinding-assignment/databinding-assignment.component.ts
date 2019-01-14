fbb703f2a937e6110b4cb04bae6f71a38d134817
m3c0d3 <m3c0d3@gmail.com>
2019-01-14 02:15:06 +0200

    Data binding assignment.

---
+++ src/app/databinding-assignment/databinding-assignment.component.ts
@@ -0,0 +1,25 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-databinding-assignment',
+  templateUrl: './databinding-assignment.component.html',
+  styleUrls: ['./databinding-assignment.component.css']
+})
+export class DatabindingAssignmentComponent implements OnInit {
+  username = '';
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  isButtonDisabled() {
+    return !this.username;
+  }
+
+
+  resetUsername() {
+    this.username = '';
+  }
+}
